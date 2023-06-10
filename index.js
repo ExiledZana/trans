1c8a6f29e23c96283ce3e4acf1a66cb5c798762a
Leo <lenovik1@gmailcom>
2023-06-10 13:52:46 +0200

    doesnt work

--- index.js
+++ index.js
@@ -5,76 +5,94 @@ const { getRandomSender, getRandomReceiver, getRandomAmount } = require('./gener
 const folderPath = './archive/';
 const balanceFolder = './balances'
 
-const otpravitel = getRandomSender();
-const prinimatel = getRandomReceiver();
-const kolvo = getRandomAmount();
-const balancePathSender = `${balanceFolder}/user_${otpravitel}.json`;
-const balancePathReceiver = `${balanceFolder}/user_${prinimatel}.json`;
-const senderData = fs.readFileSync(balancePathSender, 'utf8');
-const receiverData = fs.readFileSync(balancePathReceiver, 'utf8');
+const balance = {};
 
-function checkBalance(){
 
-  if (!fs.existsSync(balancePathSender)){
-    const files = fs.readdirSync(folderPath);
-    let currentBalance = 0;
+function generateRandom() {
+  const sender = getRandomSender();
+  const receiver = getRandomReceiver();
+  const amount = getRandomAmount();
+  const balancePathSender = `${balanceFolder}/user_${sender}.json`;
+  const balancePathReceiver = `${balanceFolder}/user_${receiver}.json`;
+  const senderData = fs.readFileSync(balancePathSender, 'utf8');
+  const receiverData = fs.readFileSync(balancePathReceiver, 'utf8');
+
+  checkBalanceFiles(currentBalance, sender, receiver)
+
+
+
+}
+
+
+function fullBalanceScan (person){
+  const files = fs.readdirSync(folderPath);
+    const currentBalance = 0;
     files.forEach((file) => {
-    const filePath = `${folderPath}/${file}`;
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    if (fileContent.trim().length === 0) {
-      return;
-    }
-    try {
-      const jsonData = JSON.parse(fileContent);
-      if (jsonData.receiver === otpravitel) {
-        currentBalance += jsonData.amount;
+      const filePath = `${folderPath}/${file}`;
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      if (fileContent.trim().length === 0) {
+        return;
       }
-  }
-  catch (err){
-    console.error(`Error parsing JSON from file ${filePath}:`, err);
-  }})
-
-  const objectSender = {balance: currentBalance};
-  const jsonSender = JSON.stringify(objectSender);
-  fs.writeFileSync(balancePathReceiver, jsonSender);
-  };
-
-
-
-  if (!fs.existsSync(balancePathReceiver)){
-    const files = fs.readdirSync(folderPath);
-    let currentBalance = 0;
-    files.forEach((file) => {
-    const filePath = `${folderPath}/${file}`;
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    if (fileContent.trim().length === 0) {
-      return;
-    }
-    try {
-      const jsonData = JSON.parse(fileContent);
-      if (jsonData.receiver === prinimatel) {
-        currentBalance += jsonData.amount;
+      try {
+        const jsonData = JSON.parse(fileContent);
+        if (jsonData[person] === person) {
+          currentBalance += jsonData.amount;
+        }
       }
-    }
-    catch (err){
-      console.error(`Error parsing JSON from file ${filePath}:`, err);
-    }})
+      catch (err) {
+        console.error(`Error parsing JSON from file ${filePath}:`, err);
+      }
+    })
+    return(currentBalance);
+}
 
-  const objectReceiver = {balance: currentBalance};
+function balanceScanSender(currentBalance){
+  fullBalanceScan (sender);
+
+    const objectSender = { balance: currentBalance };
+    const jsonSender = JSON.stringify(objectSender);
+    fs.writeFileSync(balancePathReceiver, jsonSender);
+}
+
+function balanceScanReceiver(currentBalance){
+  fullBalanceScan (receiver);
+
+  const objectReceiver = { balance: currentBalance };
   const jsonReceiver = JSON.stringify(objectReceiver);
   fs.writeFileSync(balancePathReceiver, jsonReceiver);
+}
+
+
+function checkBalanceFiles(currentBalance) {
+
+  if (!fs.existsSync(balancePathSender) || !fs.existsSync(balancePathReceiver)) {
+    balanceScanSender(currentBalance);
+    balanceScanReceiver(currentBalance);
+  };
+
+  if (!fs.existsSync(balancePathSender)){
+    balanceScanSender(currentBalance);
   }
 
-  const senderBalanceData = JSON.parse(senderData);
-  const receiverBalanceData = JSON.parse(receiverData);
-  const senderBalance = senderBalanceData.balance;
-  const receiverBalance = receiverBalanceData.balance;
-
-  return {
-    senderBalance: senderBalance,
-    receiverBalance: receiverBalance
+  if (!fs.existsSync(balancePathReceiver)) {
+    balanceScanReceiver(currentBalance);
   }
 }
+
+function parseBalance(person) {
+  if (typeof balance.person === 'underfined'){
+    const balancePathPerson = `${balanceFolder}/user_${person}.json`;
+    const personData = fs.readFileSync(balancePathPerson, 'utf8');
+    const personBalanceData = JSON.parse(personData);
+    balance[person] = personBalanceData.balance;
+    //return (balance[person]);
+  }
+  else {
+    return;
+  }
+}
+
+
 
 function countFilesInFolder(folderPath) {
   try {
@@ -92,73 +110,82 @@ function countFilesInFolder(folderPath) {
 
 function writingTrans(folderPath) {
 
-    const balance = checkBalance();
-    const fileCount = countFilesInFolder(folderPath);
-    const currentDate = new Date();
-    const fileName = `id_${fileCount}.json`;
-    const filePath = folderPath + fileName;
-    const komissia = Math.round(kolvo*0.01);
-    const total = kolvo + komissia;
+  const fileCount = countFilesInFolder(folderPath);
+  const currentDate = new Date();
+  const fileName = `id_${fileCount}.json`;
+  const filePath = folderPath + fileName;
+  const comission = Math.round(amount * 0.01);
+  const total = amount + comission;
 
 
-    if (balance.senderBalance < kolvo + komissia){
-      console.log(`${otpravitel}'s balance is too low. Current Balance ${balance.senderBalance}. Required amount ${total}`)
-      return;
-    }
+  if (balance.senderBalance < amount + comission) {
+    console.log(`${sender}'s balance is too low. Current Balance ${balance.senderBalance}. Required amount ${total}`)
+    return;
+  }
 
-    if (otpravitel === prinimatel){
-      console.log(`Mr. ${otpravitel}, you can not send tokens to yourself`)
-      return;
-    }
+  if (sender === receiver) {
+    console.log(`Mr. ${sender}, you can not send tokens to yourself`)
+    return;
+  }
 
-    else {
-      const data = {
-        id: fileCount,
-        sender: otpravitel,
-        receiver: prinimatel,
-        amount: kolvo,
-        type: 'transfer',
-        date: currentDate,
-        comission: komissia
-      };
+  else {
+    const data = {
+      id: fileCount,
+      sender: sender,
+      receiver: receiver,
+      amount: amount,
+      type: 'transfer',
+      date: currentDate,
+      comission: comission
+    };
 
-      const jsonData = JSON.stringify(data, null, 2);
+    const jsonData = JSON.stringify(data, null, 2);
 
-      fs.writeFile(filePath, jsonData, (err) => {
-        if (err) {
-          console.error('Error writing to file:', err);
-        } else {
-          console.log(`Successful operation: ${kolvo} are sent from ${otpravitel} to ${prinimatel}. Comission is ${komissia}.`);
-        }
-      });
+    fs.writeFile(filePath, jsonData, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log(`Successful operation: ${amount} are sent from ${sender} to ${receiver}. Comission is ${comission}.`);
+      }
+    })
 
-      const newSenderBalance = balance.senderBalance - total;
-      const newReceiverBalance = balance.receiverBalance + kolvo;
-      const senderBalanceDataUpdate = JSON.parse(senderData);
-      const receiverBalanceDataUpdate = JSON.parse(receiverData);
-      senderBalanceDataUpdate.balance = newSenderBalance;
-      receiverBalanceDataUpdate.balance = newReceiverBalance;
-      const updatedSenderData = JSON.stringify(senderBalanceDataUpdate, null, 2);
-      const updatedReceiverData = JSON.stringify(receiverBalanceDataUpdate, null, 2);
-
-      fs.writeFile(balancePathSender, updatedSenderData, 'utf8', (error) => {
-        if (error) {
-          console.error('Error writing JSON file:', error);
-          return;
-        }
-        console.log(`New user ${otpravitel} balance = ${newSenderBalance}`);
-      });
-
-      fs.writeFile(balancePathReceiver, updatedReceiverData, 'utf8', (error) => {
-        if (error) {
-          console.error('Error writing JSON file:', error);
-          return;
-        }
-        console.log(`New user ${prinimatel} balance = ${newReceiverBalance}`);
-      });
+    balance[sender] -= total;
+    balance[receiver] += amount;
+  
+}}
 
 
-}};
+
+
+  function countBalance(sender, receiver){
+    //const newSenderBalance = balance.senderBalance - total;
+    //const newReceiverBalance = balance.receiverBalance + amount;
+    const senderBalanceDataUpdate = JSON.parse(senderData);
+    const receiverBalanceDataUpdate = JSON.parse(receiverData);
+    senderBalanceDataUpdate.balance = balance[sender];
+    receiverBalanceDataUpdate.balance = balance[receiver];
+    const updatedSenderData = JSON.stringify(senderBalanceDataUpdate, null, 2);
+    const updatedReceiverData = JSON.stringify(receiverBalanceDataUpdate, null, 2);
+
+    fs.writeFile(balancePathSender, updatedSenderData, 'utf8', (error) => {
+      if (error) {
+        console.error('Error writing JSON file:', error);
+        return;
+      }
+      console.log(`New user ${sender} balance = ${balance[sender]}`);
+    });
+
+    fs.writeFile(balancePathReceiver, updatedReceiverData, 'utf8', (error) => {
+      if (error) {
+        console.error('Error writing JSON file:', error);
+        return;
+      }
+      console.log(`New user ${receiver} balance = ${balance[receiver]}`);
+    });
+
+
+  }
+;
 
 
 
